f9fcb78c76d3f6fe2f901439f3bc87ebb5aa75be
= <stephen.carlylesmith@googlemail.com>
2022-06-13 21:48:44 +0100

    More rooms and scenery

--- src/Scripts/Room.gd
+++ src/Scripts/Room.gd
@@ -3,7 +3,8 @@ extends Node
 var spacestation
 
 func _ready():
-	var cls = load("Rooms/Room1.tscn") # todol - preload
+	var rnd = Globals.rnd.randi_range(1, 4)
+	var cls = load("Rooms/Room" + str(rnd) + ".tscn") # todol - preload
 	var room = cls.instance()
 	self.add_child(room)
 	pass
